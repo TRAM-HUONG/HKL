474f351c305cb39e275cc092987af151d50fd20e
TRAM-HUONG <1250080066@sv.hcmunre.edu.vn>
2026-05-04 13:45:00 +0700

    Delete CUOIKYMNM/Frontend directory

--- CUOIKYMNM/Frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // Thêm dòng này để hết lỗi "React is not defined"
-import Home from './pages/home/Home';
-
-function App() { 
-  return <Home />; 
-}
-
-export default App;
